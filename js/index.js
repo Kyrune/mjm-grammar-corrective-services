1b33308470e3d67e29b3286bbc806cf4cfb64b90
Kyrune <jn-01@centurytel.net>
2024-10-03 15:24:24 -0500

    Add functionality to top-anchor button

--- js/index.js
+++ js/index.js
@@ -10,3 +10,22 @@
 //         document.body.classList.remove('nav-open');
 //     });
 // });
+
+
+// Scroll to top functionality
+topAnchor = document.getElementById('top-anchor');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topAnchor.style.display = 'block';
+    } else {
+        topAnchor.style.display = 'none';
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
